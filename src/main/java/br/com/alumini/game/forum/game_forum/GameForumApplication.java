8d1255d19e514c9350ee24563ed2f6549cbea8e7
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2025-02-19 08:45:25 -0300

    Inicializacao aplicacao Spring

---
+++ src/main/java/br/com/alumini/game/forum/game_forum/GameForumApplication.java
@@ -0,0 +1,13 @@
+package br.com.alumini.game.forum.game_forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alumini/game/forum/game_forum/GameForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alumini.game.forum.game_forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
