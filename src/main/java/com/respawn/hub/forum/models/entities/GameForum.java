246658fb75edb4fd16599f70410d62518cad872a
Mauricio Hidani <mauricio.hidani.work@gmail.com>
2025-02-27 14:37:57 -0300

    Entidade GameForum

---
+++ src/main/java/com/respawn/hub/forum/models/entities/GameForum.java
@@ -0,0 +1,169 @@
+package com.respawn.hub.forum.models.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+@Entity
+@Table(name = "games")
+public class GameForum implements Serializable {
+
+    @Serial private static final long serialVersionUID = 1133021496152260324L;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Column private String name;
+    @Column private String originalName;
+    @Column private String description;
+    @Column private LocalDate releaseA;
+    @Column private String backgroundImageUrl;
+    @Column private String websiteUrl;
+
+    // TODO: incluir os relacionamentos
+
+    @Deprecated
+    protected GameForum() {
+    }
+
+    public GameForum(String name, String originalName, String description, LocalDate releaseA,
+                     String backgroundImageUrl, String websiteUrl) {
+        this.name = name;
+        this.originalName = originalName;
+        this.description = description;
+        this.releaseA = releaseA;
+        this.backgroundImageUrl = backgroundImageUrl;
+        this.websiteUrl = websiteUrl;
+    }
+
+    public GameForum(Long id, String name, String originalName, String description, LocalDate releaseA,
+                     String backgroundImageUrl, String websiteUrl) {
+        this.id = id;
+        this.name = name;
+        this.originalName = originalName;
+        this.description = description;
+        this.releaseA = releaseA;
+        this.backgroundImageUrl = backgroundImageUrl;
+        this.websiteUrl = websiteUrl;
+    }
+
+    public GameForum(Builder builder) {
+        this.id = builder.id;
+        this.name = builder.name;
+        this.originalName = builder.originalName;
+        this.description = builder.description;
+        this.releaseA = builder.release;
+        this.backgroundImageUrl = builder.backgroundImageUrl;
+        this.websiteUrl = builder.websiteUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getReleaseA() {
+        return releaseA;
+    }
+
+    public String getBackgroundImageUrl() {
+        return backgroundImageUrl;
+    }
+
+    public String getWebsiteUrl() {
+        return websiteUrl;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameForum gameForum = (GameForum) o;
+        return Objects.equals(name, gameForum.name) &&
+                Objects.equals(originalName, gameForum.originalName) &&
+                Objects.equals(releaseA, gameForum.releaseA);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, originalName, releaseA);
+    }
+
+    @Override
+    public String toString() {
+        return "GameForum { " +
+                "id: %d" +
+                ", name: '%s'" +
+                ", originalName: '%s'" +
+                ", description: '%s'" +
+                ", release: '%s'" +
+                ", backgroundImageUrl: '%s'" +
+                ", websiteUrl: '%s'" +
+                "}".formatted(id, name, originalName, description, releaseA, backgroundImageUrl, websiteUrl);
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String originalName;
+        private String description;
+        private LocalDate release;
+        private String backgroundImageUrl;
+        private String websiteUrl;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder originalName(String originalName) {
+            this.originalName = originalName;
+            return this;
+        }
+
+        public Builder description(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder release(LocalDate release) {
+            this.release = release;
+            return this;
+        }
+
+        public Builder backgroundImageUrl(String backgroundImageUrl) {
+            this.backgroundImageUrl = backgroundImageUrl;
+            return this;
+        }
+
+        public Builder websiteUrl(String websiteUrl) {
+            this.websiteUrl = websiteUrl;
+            return this;
+        }
+
+        public GameForum build() {
+            return new GameForum(this);
+        }
+    }
+}
